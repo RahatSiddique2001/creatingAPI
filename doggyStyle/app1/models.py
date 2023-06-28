fcdff414001a31a5f5b777f725f1fb9b0abf69a5
Rahat Siddique <rahatsiddique2001@gmail.com>
2023-06-28 18:45:22 +0600

    a model named Breed is created

--- doggyStyle/app1/models.py
+++ doggyStyle/app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Breed(models.Model):
+    name = models.CharField(max_length=22)
+    color = models.CharField(max_length=22)
+    size = models.PositiveIntegerField()
+    friendliness = models.CharField(max_length=22)
+
